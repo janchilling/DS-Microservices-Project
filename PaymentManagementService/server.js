9e02c72fec452c2d497a3dc82df0b3c70b925527
Keshan <janindupathi@gmail.com>
2024-05-06 20:25:05 +0530

    Folder structure

---
+++ PaymentManagementService/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 3003;
+
+// app middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json())
+
+//database connection
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL).then(() => {
+    app.listen(port, () => {
+        console.log(`server is up and running on port number ${port}`)
+    })
+}).catch((error) => {
+    console.log(error)
+})
+
+
+// const userRouter = require("./routes/user.js");
+// app.use("/user",authenticate, userRouter);
+
+
+
---
+++ api-gateway/gateway.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const proxy = require("express-http-proxy");
+
+const app = express();
+
+app.use(express.json());
+
+// Set up CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+// Proxy requests to different services
+app.use("/api/payment", proxy("http://payment-management-service:3003"));
+
+// Start the API Gateway
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Gateway is Listening to Port ${PORT}`);
+});
