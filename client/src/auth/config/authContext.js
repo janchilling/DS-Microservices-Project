14213183767c0fc60b98fd94e09a3244b891a35f
Bihesha Dilshan <132125114+bihesha@users.noreply.github.com>
2024-05-07 22:07:44 +0530

    landing page some parts are completed.

--- client/src/auth/config/authContext.js
+++ client/src/auth/config/authContext.js
@@ -1,52 +1,52 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../../config/firebase";
-import { useNavigate } from "react-router-dom";
+// import React, { createContext, useContext, useEffect, useState } from "react";
+// import { auth } from "../../config/firebase";
+// import { useNavigate } from "react-router-dom";
 
-const AuthContext = createContext();
+// const AuthContext = createContext();
 
-export const useAuth = () => useContext(AuthContext);
+// export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+// export const AuthProvider = ({ children }) => {
+//     const [currentUser, setCurrentUser] = useState(null);
+//     const [loading, setLoading] = useState(true);
+//     const navigate = useNavigate();
 
-    const signup = (email, password) => {
-        return auth.createUserWithEmailAndPassword(email, password);
-    };
+//     const signup = (email, password) => {
+//         return auth.createUserWithEmailAndPassword(email, password);
+//     };
 
-    const login = async (email, password) => {
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            navigate("/");
-        } catch (error) {
-            throw error;
-        }
-    };
+//     const login = async (email, password) => {
+//         try {
+//             await auth.signInWithEmailAndPassword(email, password);
+//             navigate("/");
+//         } catch (error) {
+//             throw error;
+//         }
+//     };
 
-    const logout = () => {
-        return auth.signOut();
-    };
+//     const logout = () => {
+//         return auth.signOut();
+//     };
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
-            setLoading(false);
-        });
+//     useEffect(() => {
+//         const unsubscribe = auth.onAuthStateChanged(user => {
+//             setCurrentUser(user);
+//             setLoading(false);
+//         });
 
-        return unsubscribe;
-    }, []);
+//         return unsubscribe;
+//     }, []);
 
-    const value = {
-        currentUser,
-        signup,
-        login,
-        logout
-    };
+//     const value = {
+//         currentUser,
+//         signup,
+//         login,
+//         logout
+//     };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    );
-};
+//     return (
+//         <AuthContext.Provider value={value}>
+//             {!loading && children}
+//         </AuthContext.Provider>
+//     );
+// };
