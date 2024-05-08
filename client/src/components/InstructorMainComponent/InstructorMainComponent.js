bbc0d4184134753b8b709dde46fb48197c900628
Bihesha Dilshan <132125114+bihesha@users.noreply.github.com>
2024-05-09 00:12:56 +0530

    Instructor main component updated.

--- client/src/components/InstructorMainComponent/InstructorMainComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const InstructorMain = () => {
-    return (
-        <div>This is Instructor Main Page....!</div>
-    )
-}
-
-export default InstructorMain;
--- client/src/pages/homePage/homePage.js
+++ client/src/pages/homePage/homePage.js
@@ -3,7 +3,7 @@ import Main from "../../components/mainComponent/mainComponent";
 import Features from "../../components/featuresComponent/featuresComponent";
 import Testimonial from "../../components/testimonialComponent/testimonialComponent";
 import CallToAction from "../../components/callToActionComponent/callToActionComponent";
-import InstructorMain from "../../components/InstructorMainComponent/InstructorMainComponent";
+import InstructorMain from "../../components/instructorMainComponent/instructorMain";
 import UserContext from '../../ContextComponent/ContextComponent';
 
 export default function Home() {
