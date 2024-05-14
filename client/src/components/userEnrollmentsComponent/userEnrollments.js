64f9446ab2503b5605a87c39eb8e02b8dd7f1df5
ManulaGunatilleke <inner.gunatilleke@gmail.com>
2024-05-14 21:25:26 +0530

    Added yaml files with gateway and userEnrollment Updates

--- client/src/components/userEnrollmentsComponent/userEnrollments.js
+++ client/src/components/userEnrollmentsComponent/userEnrollments.js
@@ -77,7 +77,7 @@ const UserEnrollments = () => {
                             </div> */}
                             <div className="button-container">
                                 <button onClick={() => enrollCourse(enrollment.courseId)} className="mt-4 bg-orange-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                    view course
+                                    View course
                                 </button>
                             </div>
                         </div>
